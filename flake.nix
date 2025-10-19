3fa38cc2237dc9f61f4b898d7b1fbc12be0f8772
Enpassant <enpassant.prog@gmail.com>
2025-10-19 17:43:15 +0200

    Add maven project

---
+++ flake.nix
@@ -0,0 +1,100 @@
+{
+  description = "Build Maven project using Nix Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      lib = pkgs.lib;
+      jdk = pkgs.openjdk11;
+      jre = pkgs.jre11_minimal;
+    in {
+      artifacts = pkgs.stdenv.mkDerivation {
+        name = "maven-repository";
+        buildInputs = [ pkgs.maven ];
+        src = ./.; # or fetchFromGitHub, cleanSourceWith, etc
+        buildPhase = ''
+          mvn clean package -Dmaven.repo.local=$out
+        '';
+
+        # keep only *.{pom,jar,sha1,nbm} and delete all ephemeral files with lastModified timestamps inside
+        installPhase = ''
+          find $out -type f \
+            -name \*.lastUpdated -or \
+            -name resolver-status.properties -or \
+            -name _remote.repositories \
+            -delete
+        '';
+
+        # don't do any fixup
+        dontFixup = true;
+        outputHashAlgo = "sha256";
+        outputHashMode = "recursive";
+        # replace this with the correct SHA256
+        # outputHash = lib.fakeSha256;
+        outputHash = "sha256-Ja32fkF/l+Lx3Wkxaac4vY+86AMkIe/Pw4vf+ZrtiZ4=";
+      };
+
+      packages.${system}.default = let
+        repository = self.artifacts;
+      in pkgs.stdenv.mkDerivation {
+        pname = "hello-world";
+        version = "1.0.0";
+        src = ./.;
+        # src = lib.cleanSource ./.;
+
+        nativeBuildInputs = [ pkgs.maven jdk ];
+        buildInputs = [
+          jdk
+          # pkgs.fetchMavenDeps
+        ];
+      
+        buildPhase = ''
+          export JAVA_HOME=${jdk.home}
+          echo "Building Maven project..."
+          mvn -Dmaven.repo.local=${repository} -DskipScm=true -DskipTests clean package
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp target/*.jar $out/ || echo "No JARs found in target/"
+        '';
+
+        meta = with pkgs.lib; {
+          description = "Maven project built with Nix flakes";
+          license = licenses.mit;
+        };
+      };
+
+      devShells.${system}.default = let
+        repository = self.artifacts;
+      in pkgs.mkShell {
+        buildInputs = [ pkgs.maven jdk ];
+        shellHook = ''
+          export JAVA_HOME=${jdk.home}
+          alias mvn='mvn -Dmaven.repo.local=${repository}'
+          alias build='mvn clean package'
+          echo "Maven dev shell ready. Run 'mvn package' manually if needed."
+        '';
+      };
+
+      # Usage: nix build -v ~/projects/java/hello#dockerImage -o res && ./res | podman load
+      dockerImage = pkgs.dockerTools.streamLayeredImage {
+        name = "hello";
+        tag = "latest";
+        created = "2025-10-19T10:00:01Z";
+        contents = [
+          jre
+          self.packages.x86_64-linux.default
+          pkgs.dockerTools.binSh
+        ];
+        config = {
+          Cmd = [ "/bin/java" "-jar" "/hello-world-1.0.0.jar" ];
+        };
+      };
+    };
+}
